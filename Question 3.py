9a262a68e88daa9c45726399535f603957c9e263
DrashtiShah23 <drashtishah2307@gmail.com>
2022-05-06 16:25:57 -0700

    Part a,b,c,d working but e anf f commented out due to some small error

--- Question 3.py
+++ Question 3.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sympy as sp
 import scipy.linalg as la
+from scipy.linalg import lu, lu_factor, lu_solve
 
 
 
@@ -146,8 +147,9 @@ def partB():
          [0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 0, 0, 0, 0, 0, 0],  # pxy(1, 0)
          [0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 2, 4, 6, 3, 6, 9]  # pxy(1, 1)
          ]
+
     print(np.array(b))
-  
+    # piv, lu = lu_factor(np.array(b))
     P, L, U = la.lu(np.array(b))
     np.dot(P.T, b)
     np.dot(L, U)
@@ -168,4 +170,135 @@ print("Part B:")
 partB()
 
 
-    
+def Bicubic_Interpolation(f, interpolation_of_x, interpolation_of_y, lu,piv, h):
+    def fx(x, y):
+        return (f(x+h, y) - f(x-h, y))/(2*h)
+
+    def fy(x, y):
+        return (f(x, y+h) - f(x, y-h))/(2*h)
+
+    def fxy(x, y):
+        return (f(x+h, y+h) - f(x+h, y-h)-f(x-h, y+h) + f(x-h, y-h))/(4*h**2)
+
+    right_side_f_vec = np.array([
+        f(0, 0),
+        f(1, 0),
+        f(0, 1),
+        f(1, 1),
+        fx(0, 0),
+        fx(1, 0),
+        fx(0, 1),
+        fx(1, 1),
+        fy(0, 0),
+        fy(1, 0),
+        fy(0, 1),
+        fy(1, 1),
+        fxy(0, 0),
+        fxy(1, 0),
+        fxy(0, 1),
+        fxy(1, 1),
+    ])  
+
+    alpha = lu_solve((lu, piv), right_side_f_vec)
+    p = np.dot(alpha.T, np.array)([
+        1,
+        interpolation_of_x,
+        interpolation_of_x ** 2,
+        interpolation_of_x ** 3,
+        interpolation_of_y,
+        interpolation_of_y ** 2,
+        interpolation_of_y ** 3,
+        interpolation_of_x * interpolation_of_y,
+        interpolation_of_x ** 2 * interpolation_of_y,
+        interpolation_of_x ** 3 * interpolation_of_y,
+        interpolation_of_x * interpolation_of_y ** 2,
+        interpolation_of_x ** 2 * interpolation_of_y ** 2,
+        interpolation_of_x ** 3 * interpolation_of_y ** 2,
+        interpolation_of_x * interpolation_of_y ** 3,
+        interpolation_of_x ** 2 * interpolation_of_y ** 3,
+        interpolation_of_x ** 3 * interpolation_of_y ** 3
+    ])
+
+    px = np.dot(alpha.T, np.array)([
+        0,
+        1,
+        2 * interpolation_of_x,
+        3 * interpolation_of_x ** 2,
+        0,
+        0,
+        0,
+        interpolation_of_y,
+        2 * interpolation_of_x * interpolation_of_y,
+        3 * interpolation_of_x ** 2 * interpolation_of_y,
+        interpolation_of_y ** 2,
+        2 * interpolation_of_x * interpolation_of_y ** 2,
+        3 * interpolation_of_x ** 2 * interpolation_of_y ** 2,
+        interpolation_of_y ** 3,
+        2 * interpolation_of_x * interpolation_of_y ** 3,
+        3 * interpolation_of_x ** 2 * interpolation_of_y ** 3
+    ])
+
+    py = np.dot(alpha.T,np.array)([
+        0,
+        0,
+        0,
+        0,
+        1,
+        2 * interpolation_of_y,
+        3 * interpolation_of_y ** 2,
+        interpolation_of_x,
+        interpolation_of_x ** 2,
+        interpolation_of_x ** 3,
+        2 * interpolation_of_x * interpolation_of_y,
+        2 * interpolation_of_x ** 2 * interpolation_of_y,
+        2 * interpolation_of_x ** 3 * interpolation_of_y,
+        3 * interpolation_of_x * interpolation_of_y ** 2,
+        3 * interpolation_of_x ** 2 * interpolation_of_y ** 2,
+        3 * interpolation_of_x ** 3 * interpolation_of_y ** 2
+    ])
+
+    pxy = np.dot(alpha.T, np.array)([
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        1,
+        2 * interpolation_of_x,
+        3 * interpolation_of_x ** 2,
+        2 * interpolation_of_y,
+        4 * interpolation_of_x * interpolation_of_y,
+        6 * interpolation_of_x ** 2 * interpolation_of_y,
+        3 * interpolation_of_y ** 2,
+        6 * interpolation_of_x * interpolation_of_y ** 2,
+        9 * interpolation_of_x ** 2 * interpolation_of_y ** 2
+
+    ])
+    interpolation = [p, px, py, pxy]
+
+    # Calculating the error
+
+    approx_centered_difference = [f(interpolation_of_x, interpolation_of_y), fx(interpolation_of_x, interpolation_of_y), fy(
+        interpolation_of_x, interpolation_of_y), fxy(interpolation_of_x, interpolation_of_y)]
+    error = np.subtract(approx_centered_difference, interpolation)
+    inf_norm = abs[max(error)]
+    one_norm = sum([abs(x) for x in error])
+    two_norm = np.sqrt(sum([x**2 for x in error]))
+    norms = [inf_norm, one_norm, two_norm]
+
+    return interpolation, error, norms
+
+    # interpolation for part e
+# interpolation_j, error_j, norms_j = Bicubic_Interpolation(lambda x,y: (y**2*(np.e**(-(x**2 + y**2)))),0.5, 0.5, lu, piv, 0.01)
+# print("Interpolation:", Interpolation)
+# print("Error: ", error)
+# print("norms:", norms)
+
+
+# interpolation for part f
+# interpolation_g, error_g, norms_g = Bicubic_Interpolation(lambda x,y: (x**2*(np.tanh(x*y))),0.5, 0.5, lu, piv, 0.01)
+# print("Interpolation:", Interpolation)
+# print("Error: ", error)
+# print("norms:", norms)
